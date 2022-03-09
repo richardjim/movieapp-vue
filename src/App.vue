3e3b5e8711f7aa741b1df7a6a38f11f9b506d169
Richard iyevhobu <Richardiyevhobu@gmail.com>
2022-03-09 01:16:17 +0100

    i create

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+  <router-link to="/">
+  <h1><span>Search</span>Movies</h1>
+  </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'fira sans', sans-serif;
+
+  &::selection{
+    background: transparentize($color: #42b883, $amount: 0.5);
+  }
+}
+body{
+  background-color: #35495E;
 }
 
-nav {
-  padding: 30px;
+a{
+  text-decoration: none;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+header{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3d4e;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
 }
 </style>
